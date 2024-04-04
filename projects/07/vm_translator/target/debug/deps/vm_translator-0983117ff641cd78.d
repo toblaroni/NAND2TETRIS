5a200a6ca6079df8b23975acfd2680c4408f1932
Toby Hutchinson <tobyhutchinson@gmail.com>
2024-04-04 11:22:00 +0100

    Remove target/ from git

--- projects/07/vm_translator/target/debug/deps/vm_translator-0983117ff641cd78.d
+++
@@ -1,5 +0,0 @@
-C:\Users\gbhutchinsont\Documents\NAND2TETRIS\projects\07\vm_translator\target\debug\deps\libvm_translator-0983117ff641cd78.rmeta: src\main.rs
-
-C:\Users\gbhutchinsont\Documents\NAND2TETRIS\projects\07\vm_translator\target\debug\deps\vm_translator-0983117ff641cd78.d: src\main.rs
-
-src\main.rs:
